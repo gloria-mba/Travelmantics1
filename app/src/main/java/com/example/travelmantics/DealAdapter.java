723ee8e2af2dc0747a94348e4c9ebb0d2ab885d8
luciamcalmer <lucymba06m@gmail.com>
2019-08-07 13:04:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/travelmantics/DealAdapter.java
@@ -0,0 +1,79 @@
+package com.example.travelmantics;
+
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class DealAdapter extends RecyclerView.Adapter {
+    ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildListener;
+    private ImageView imageDeal;
+
+    public DealAdapter() {
+        //FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        this.deals = FirebaseUtil.mDeals;
+        mChildListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
+                Log.d("Deal: ", td.getTitle());
+                td.setId(dataSnapshot.getKey());
+                deals.add(td);
+                notifyItemInserted(deals.size()-1);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildListener);
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
